17aec1e86d4703da58e60eee3f0b3b85a10a7fd6
chelseacastelli <chelsea.castelli@students.makeschool.com>
2020-06-08 19:31:48 -0400

    [UPDATE] Create overall design

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by ChelseaAnne Castelli on 6/8/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
